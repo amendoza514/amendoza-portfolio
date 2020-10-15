41f43b32acebfffbc9bdeed20d481704b1a25496
amendoza514 <amendoza15@cmc.edu>
2020-10-15 16:05:14 -0700

    navbar outline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa' 
+import { Button } from './Button'
+import './Navbar.css'
 
 function Navbar() {
+    let [click, setClick] = useState(false)
+    let [button, setButton] = useState(true);
+
+    let handleClick = () => setClick(!click);
+    let closeMenu = () => setClick(false);
+
+    let showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton)
+
     return (
-        <>
-          <div className="navbar">
-              <div className="navbar-container container">
-                <Link to='/' className="navbar-logo">
-                    WELCOME
-                    <MdFingerprint className='navbar-icon' />
+      <>
+        <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo">
+              <MdFingerprint className="navbar-icon" />
+              WELCOME
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links">
+                  Home
                 </Link>
-                <div className="menu-icon">
-                    
-                </div>
-              </div>
+              </li>
+              <li className="nav-item">
+                <Link to="/projects" className="nav-links">
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-button">
+                {button ? (
+                  <Link to="/signup" className="button-link">
+                    <Button buttonStyle="button--outline">Sign Up</Button>
+                  </Link>
+                ) : (
+                  <Link to="/signup" className="button-link">
+                    <Button
+                      buttonStyle="button--outline"
+                      buttonSize="button--mobile"
+                    >
+                      Sign Up Mobile
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
           </div>
-        </>
-    )
+        </div>
+      </>
+    );
 }
 
 export default Navbar
